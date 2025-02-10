c8f6f1b1316f9f56941a521f505b4fc54a4ef5c0
Ravindu-DeAlwis <ravinduweerasiriwardhana@gmail.com>
2025-02-10 16:04:31 +0200

    Initial commit for NextGenTech Wagtail project

---
+++ NextGenTech/NextGenTech/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-x!xvio%8vuh89r6=urp@fle4s%%nf4s0zkr+z1db7q48looj@2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
